e07dcd751aa522360a3a814d2fe573bc6b0b92aa
Konstantinos Pliakas <kjpliakas@gmail.com>
2016-07-25 17:18:06 +0300

    Dude..FIX IT ALREADY
    For god's sake...

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -31,11 +31,11 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.qcom.fm.rc
-LOCAL_SRC_FILES := init.qcom.fm.rc
+LOCAL_MODULE := init.qcom.fm.sh
+LOCAL_SRC_FILES := init.qcom.fm.sh
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.qcom.fm.rc
-LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_STEM := init.qcom.fm.sh
+LOCAL_MODULE_SUFFIX := .sh
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
